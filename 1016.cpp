fe86ed89c1670203663b3a09f1856558cbcd2409
Roberto Tetsuo <>
2021-05-05 19:32:09 +0000

    1016

---
+++ 1016.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     
+     int y;
+     cin >> y;
+     cout << y * 2 << " minutos" << endl;
+ 
+    return 0;
+}
