56a2990c2b9cf29a9b41394baa4acb351525ce9c
Roberto Tetsuo <>
2021-05-05 19:16:22 +0000

    1013

---
+++ 1013.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, maiorAB, maior;
+    cin >> a >> b >> c;
+    maiorAB = (a+b+abs(a-b))/2;
+    maior = (maiorAB+c+abs(maiorAB-c))/2;
+    cout << maior << " eh o maior" << endl;
+
+    return 0;
+} 
